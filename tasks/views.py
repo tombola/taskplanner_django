c15225df4b2fad126df7f6fce1e1f2e5fba0fa0c
tombola <tombola@github>
2026-02-11 17:13:12 +0000

    Improve task group template ui

--- tasks/views.py
+++ tasks/views.py
@@ -10,6 +10,14 @@ from .models import CropTaskGroupTemplate
 class TaskGroupTemplateCRUDView(LoginRequiredMixin, CRUDView):
     model = CropTaskGroupTemplate
     fields = ["title", "description", "project_id", "tasks"]
+    url_base = "taskgrouptemplate"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        parent_task_class = self.model.parent_task_class
+        if parent_task_class:
+            context["token_names"] = parent_task_class.get_token_field_names()
+        return context
 
 
 def home(request):
